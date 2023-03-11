88e83363d62c4f2585054e266ee77b503d88fae0
최서윤 <cinnamon1995@naver.com>
2023-03-11 15:46:33 +0900

    툴킷을 이용해 간결하게 끝내다

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { createStore } from 'redux'
 import { combineReducers } from 'redux'
 import counter from '../modules/counter'
 
-const rootReducer = combineReducers({
-  counter: counter,
-})
+// ASIS : 일반 리듀서
+// const rootReducer = combineReducers({
+//   counter: counter,
+// })
 
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
+
+//TODO : REdux Toolkit
+const store = configureStore({
+  reducer: {
+    counter: counter,
+  },
+})
 export default store
